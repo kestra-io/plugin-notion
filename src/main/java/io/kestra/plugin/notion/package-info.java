bceae9764e6610cdfa3fa8de9744f9e488496d42
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-07 16:00:46 +0530

    doc: add missing package-info.java

--- src/main/java/io/kestra/plugin/notion/package-info.java
+++ src/main/java/io/kestra/plugin/notion/package-info.java
@@ -1,8 +1,9 @@
 @PluginSubGroup(
     title = "Notion",
-    description = "This sub-group of plugins contains tasks for using Notion API to create, read, update, and archive pages with markdown content.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    description = "This sub-group of plugins contains tasks for integrating with Notion.\n" +
+        "Notion is a workspace platform for managing documents, databases, and workflows collaboratively.",
+    categories = { PluginSubGroup.PluginCategory.TOOL }
 )
 package io.kestra.plugin.notion;
 
-import io.kestra.core.models.annotations.PluginSubGroup; 
+import io.kestra.core.models.annotations.PluginSubGroup;
